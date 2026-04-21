94fd3aca1f79ef081e71a6bdf1e3bd607af2c550
dimka <dimonu4.1989@gmail.com>
2026-04-21 23:28:26 +0300

    working

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.use("/", express.static("./"))
+
 app.listen('3001', ()=>{
 	console.log('server on port 3001');
 })
 
+app.get("/", (req,res)=>{
+})
 
